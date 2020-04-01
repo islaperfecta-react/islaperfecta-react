38a5eb24ddfe6027aa87e59c96bb82304d7f2a5c
justo <justoelhumilde@gmail.com>
2020-04-01 06:21:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -271,7 +271,7 @@ class App extends React.Component {
       // else the socket will automatically try to reconnect
     })
 
-    
+
     this.socket.on('RECEIVE_MESSAGE', function(data, type, user){
         addMessage(data, type, user);
     })
@@ -324,7 +324,7 @@ class App extends React.Component {
       let nameCommand = message.indexOf('/name ') === 0
       if(nameCommand === true) message = message.substr(6)
       if(/^\s*$/g.test(message) === true) message = null //que no sean solo espacios
-      /* */
+      
       if((this.state.username === null || nameCommand === true) && message.length > 0){
         document.cookie = "username="+encodeURIComponent(message)+";max-age="+31536000+";expires="+(Date.UTC(Date.now()+31536000))
         this.setState({username: message})
